d39832566715ddf6233afa147ac0220f7d543949
Kenz <34919169+MrYKenz@users.noreply.github.com>
2018-12-26 19:43:21 +0000

    challenge 3

--- Build Tower.py
+++ Build Tower.py
@@ -1,11 +1,9 @@
 def tower_builder(n_floors):
     tower = []
     i = 1
-    # x = 1
     while i <= n_floors and n_floors > 0:
             space = ' ' * (n_floors - i)
             tower.append(space + '*' * (i*2 - 1) + space)
-            # x += 2
             i += 1
     return tower
 
