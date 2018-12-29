e3ec50c85ae929442e7030620d864520a8b88c92
Kenz <34919169+MrYKenz@users.noreply.github.com>
2018-12-29 20:22:32 +0000

    challenge 13

--- Regex validate PIN code.py
+++ Regex validate PIN code.py
@@ -4,3 +4,4 @@ def validate_pin(pin):
     # return re.match(r'^(?=(?:.{4}|.{6})$)(\d)*$', pin) != None
     return re.fullmatch("\d{4}|\d{6}", pin) != None
 
+### Shoulda Coulda Woulda ### len(pin) in (4, 6) and pin.isdigit()
