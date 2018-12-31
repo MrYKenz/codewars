05e9b0c931b421a12a5f88c5463fabac15680463
Kenz <34919169+MrYKenz@users.noreply.github.com>
2018-12-31 16:38:31 +0000

    Update Valid Parentheses.py

--- Valid Parentheses.py
+++ Valid Parentheses.py
@@ -1,6 +1,11 @@
 def valid_parentheses(string):
     #### return bool(string.count("(") == string.count(")") and len(string) in range(0,101))
-    return [i for i in string if i is "(" or i is ")"]
+    count = 0
+    for i in string:
+        if i == "(": count += 1
+        elif i == ")": count -= 1
+        if count < 0: return False
+    return count == 0
 
 print(valid_parentheses("hi())("))  
             
