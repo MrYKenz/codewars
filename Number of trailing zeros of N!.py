1ba0df05d8f4e881749cb14faafc5da149275f1a
Kenz <34919169+MrYKenz@users.noreply.github.com>
2019-03-23 21:02:12 +0000

    challenge 26

---
+++ Number of trailing zeros of N!.py
@@ -0,0 +1,27 @@
+### ATTEMPT 1 ###
+s = str(f)[::-1]
+zeros = 0
+i = 0
+while s[i] == '0':
+    zeros += 1
+    i += 1
+print(zeros)
+
+### ATTEMPT 2 ###
+import math
+len([i for i in str(math.factorial(n))[::-1] if i == '0'])
+
+### ATTEMPT 3###
+import math
+a = len(math.factorial(n))-len(math.factorial(n).rstrip('0'))
+
+##### FINAL SOLUTION ####
+# REDUCE RUNTIME BY USING PRIME FACTOR 5 IN n! #
+# Legendre's Formula A.K.A De Polignac's Formula #
+def zeros(x):
+    i = 5
+    zeros = 0
+    while x >= i:
+        zeros += x // i
+        i *= 5
+    return zeros
